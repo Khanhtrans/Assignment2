3ef122b51ab035490ee38808a341d69c84121f62
Khanhtrans <83150174+Khanhtrans@users.noreply.github.com>
2021-09-01 15:01:51 +0700

    asm

--- assignment.js
+++ assignment.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const { ObjectId } = require('mongodb');
 const app = express()
 
 const { addProduct, deleteProduct
@@ -16,40 +17,21 @@ app.use(session({
     cookie: { maxAge: 600000 }
 }));
 
-app.post('/add', async (req, res) => { // gọi hàm add
+app.post('/add', async (req, res) => { 
     const nameInput = req.body.txtName;
     const priceInput = req.body.txtPrice;
-    //kiểm tra xem nhập $ hay chưa
-    // if (priceInput.slice(-1) !== "$") {
-    //     res.render("index", {errorMsg: 'Nguoi dung chua nhap don vi tien te'});
-    //     return;
-    // }
-    const newProduct = { Name: nameInput, Price: priceInput} //khởi tạo 1 hàm newproduct để lưu sau khi thêm vào
-    //req.body là liên kết vớidòng 18, 22 bên index
-    // nếu tên nhỏ hơn 3 kí tự sẽ quay lại trang index và báo lỗi
-    // if (nameInput.length < 4) { 
-    //     res.render("index", { errorMsg: 'name less than 4 characters' })
-    //     return;
-    // }
-
-    // price quá 1000
-    // if(priceInput.length < 1000){ 
-    //     res.render("index",{errorPri:'quá 1000'})
-    //     // return;
-    // }
-
-    //chữ cái đầu tiên ko là số
-    // const fw = nameInput.chartAt(0);
-    // if(!isNaN(0)){
-    //     res.render('index',{errorFw:"your first char have to word"});
-    //     return;
-    // }
+    const categoryInput = req.body.txtCategory;
+    if(categoryInput != "Sach" && categoryInput != "lego"){
+        res.render("index", { errorMsg: 'category: sach or lego' })
+        return;
+    }
+    const newProduct = { Name: nameInput, Price: priceInput, Category:categoryInput } 
     await addProduct(newProduct);
     res.redirect("/");
 })
 
 app.get('/delete', async (req, res) => {
-    const id = req.query.id; //lấy dữ liệu từ database(id lấy từ database)
+    const id = req.query.id;
     await deleteProduct(id);
     res.redirect("/");
 })
@@ -81,18 +63,13 @@ app.post('/doLogin', async (req, res) => {
 })
 
 app.get('/', checkLogin, async (req, res) => {
-    // if(req.session["User"] == null){// kiểm tra xem người dùng đăng nhạp hay chưa
-    //     res.redirect('/login')
-    // } //allProducts là một mảng
     const dbo = await getDB();
     const allProducts = await dbo.collection("products").find({}).toArray();
-
-    res.render('index', { data: allProducts, auth: req.session["User"] })
-    // res.render('index', {data: allProducts.slice(0,2)}); //hien thi 2 san pham dau tien
+    res.render('index', { data: allProducts, auth: req.session["User"]});
 })
 
 function checkLogin(req, res, next) {
-    if (req.session["User"] == null) {// kiểm tra xem người dùng đăng nhạp hay chưa
+    if (req.session["User"] == null) {
         res.redirect('/login')
     } else {
         next();
--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 const { ObjectId, MongoClient } = require('mongodb');
 const url = "mongodb+srv://KhanhTran:Kt29888701@cluster0.uxw0c.mongodb.net/test";
+// const url = "mongodb:localhost:27017";
 
 async function getDB() {
     const client = await MongoClient.connect(url);
@@ -8,36 +9,15 @@ async function getDB() {
 }
 
 async function addProduct(newProduct) {
-    // const client = await MongoClient.connect(url);
-    // const dbo = client.db("Assignment");
     const dbo = await getDB();
     await dbo.collection("products").insertOne(newProduct);
 }
 
-
 async function deleteProduct(id) {
-    // const client = await MongoClient.connect(url);
-    // const dbo = client.db("Assignment");
     const dbo = await getDB();
-    await dbo.collection("products").deleteOne({ "_id": ObjectId(id) });
+    await dbo.collection("products").deleteOne({ "_id": ObjectId(id)});
 }
 
-// async function getProductById(idInput) {
-//     const dbo = await getDB()
-//     // return dbo.collection("products").findOne({ _id: ObjectId(idInput) })
-//     const search = await dbo.collection("products").findOne({ _id: ObjectId(id) });
-//     return search;
-// }
-
-// async function updateProductById(id, nameInput, priceInput) {
-//     // const dbo = await getDB()
-//     dbo.collection("products").updateOne({ _id: ObjectId(id) }, { $set: { Name: nameInput, Price: priceInput } })
-//     // const filter = { _id: ObjectId(id) };
-//     // const newValue = { $set: { name: nameInput, age: ageInput, priceInput } };
-
-//     const dbo = await getDB ();
-//     await dbo.collection("products").updateOne(filter, newValue);
-// }
 async function getRole(nameInput,pass){
     const dbo = await getDB();
     const s = await dbo.collection("users").findOne({ name: nameInput, pass:pass });
